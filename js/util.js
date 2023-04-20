af047c6d03f6acc15cfca0b0664c323dca048ed7
余国平 <applefish1996@163.com>
2023-04-20 11:26:47 +0800

    updata

---
+++ js/util.js
@@ -0,0 +1,6 @@
+var time = new function(){
+    this.callbackUrl =function(){
+         setTimeout(function(){window.location.href="http://127.0.0.1:8080/view/home/home.html";}, 120000) 
+    }
+   
+}
